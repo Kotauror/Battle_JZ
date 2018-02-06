d8b73e19049463ff42b1dad8fc6edfd2f4fcab7f
kotpsot <zygmunt.justyna@gmail.com>
2018-02-06 16:10:25 +0000

    post redirect pattern

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     # 'Testing infrastructure works!'
     erb(:index)
@@ -8,10 +10,17 @@ class Battle < Sinatra::Base
   post '/player_names' do
     @player1 = params[:player1_name]
     @player2 = params[:player2_name]
+    session[:players_names] = [@player1, @player2]
+    redirect "/battle_arena"
+  end
+
+  get '/battle_arena' do
+    @player1 = session[:players_names][0]
+    @player2 = session[:players_names][1]
     erb(:player_names)
   end
-  # start the server if ruby file executed directly and it is - we call app.rb file in config.ru
+    # start the server if ruby file executed directly and it is - we call app.rb file in config.ru
 
 
-  run! if app_file == $0 # not required when we have config.ru - config.ru runs the program when we use 
+  run! if app_file == $0 # not required when we have config.ru - config.ru runs the program when we use
 end
