5a52543151e31395fe2e874109482a6dec609a2d
Daniel Keen <keendaniel01@gmail.com>
2018-02-06 16:48:30 +0000

    Completed viewing hitpoints

--- spec/features/player_form_spec.rb
+++ spec/features/player_form_spec.rb
@@ -6,4 +6,13 @@ feature 'form for entering names' do
     click_button("Submit")
     expect(page).to have_content('Daniel vs Justyna')
   end
+
+  scenario 'it shows players HP' do
+    visit('/')
+    fill_in('player1_name', with: "Daniel")
+    fill_in('player2_name', with: "Justyna")
+    click_button("Submit")
+    expect(find('div.player1-hp')).to have_content('100')
+    expect(find('div.player2-hp')).to have_content('100')
+  end
 end
