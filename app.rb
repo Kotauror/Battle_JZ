2f6f697dc0575c479aa661380afcecde30c9bf43
kotpsot <zygmunt.justyna@gmail.com>
2018-02-07 15:25:55 +0000

    PLAYER CLASSgit status1111 working

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative './lib/player'
 class Battle < Sinatra::Base
   enable :sessions
 
@@ -7,21 +8,21 @@ class Battle < Sinatra::Base
   end
 
   post '/player_names' do
-    @player1 = params[:player1_name]
-    @player2 = params[:player2_name]
-    session[:players_names] = [@player1, @player2]
+    $player1 = Player.new(params[:player1_name])
+    $player2 = Player.new(params[:player2_name])
+    # session[:players_names] = [@player1, @player2]
     redirect "/battle_arena"
   end
 
   get '/battle_arena' do
-    @player1 = session[:players_names][0]
-    @player2 = session[:players_names][1]
+    #@player1 = session[:players_names][0]
+    #@player2 = session[:players_names][1]
     erb(:player_names)
   end
 
   get '/attack' do
-    @player1 = session[:players_names][0]
-    @player2 = session[:players_names][1]
+    #@player1 = session[:players_names][0]
+  #  @player2 = session[:players_names][1]
     erb(:attack)
   end
 
