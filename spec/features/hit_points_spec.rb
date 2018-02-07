caa8d156d183039610432e01aa8041d086870ed7
Kaari Strack <kaari.strack@gmail.com>
2018-02-07 16:14:40 +0000

    fixed names

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -4,4 +4,10 @@ feature 'hit points' do
     expect(find('div.player1-hp')).to have_content('100')
     expect(find('div.player2-hp')).to have_content('100')
   end
+
+  # scenario 'it descreases players HP' do
+  #   sign_in_and_play
+  #   click_button('Attack Justyna')
+  #   expect(find('div.player2-hp')).to have_content('90')
+  # end
 end
