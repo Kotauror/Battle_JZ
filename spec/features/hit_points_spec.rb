5a3a90a258aac8db05e02caf4264d30a8a20485c
kotpsot <zygmunt.justyna@gmail.com>
2018-02-07 22:57:25 +0000

    fix css

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -8,7 +8,7 @@ feature 'hit points' do
   scenario 'it decreases players HP' do
     sign_in_and_play
     click_button('Attack Justyna')
-    click_button('Go back')
+    click_button('Go back to the battle arena')
     expect(find('div.player2-hp')).to have_content('90')
   end
 end
