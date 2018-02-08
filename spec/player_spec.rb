bcb9274571b46cab8dc943883506b624d79f6d16
kotpsot <zygmunt.justyna@gmail.com>
2018-02-08 13:45:03 +0000

    create new Game class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,8 +4,15 @@ describe Player do
   subject(:player) { described_class.new(name) }
   let(:name) {double("name of player")}
 
+  it 'has a name' do
+    expect(player.name).to eq name
+  end
 
   it 'has a name' do
     expect(player.name).to eq name
+  end
+
+  it 'receives damage' do
+    expect{player.receive_damage}.to change{player.hp}.by(-10)
   end
 end
